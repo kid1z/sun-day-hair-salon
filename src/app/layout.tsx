7d1db528c45f95e8e0c3df4e09741cbdb58689de
hiep-tran-tpv-clv <hiep.tran.tpv@one-line.com>
2026-05-02 10:54:56 +0700

    chore: update theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,7 +76,7 @@ export default function RootLayout({
           <TooltipProvider>
             <ThemeProvider
               attribute="class"
-              defaultTheme="system"
+              defaultTheme="dark"
               enableSystem
               disableTransitionOnChange
             >
